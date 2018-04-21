bddec7bd2c533fa4aad07bb7ab29b8a8d648ce84
Ingrid Laurikainen <ingridlaurikainen@gmail.com>
2018-04-21 23:02:49 +0300

    Proov

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("proov");
+
         Tron game = new Tron();
         Scanner sc = new Scanner(System.in);
         //Mängijate informatsiooni küsimine
