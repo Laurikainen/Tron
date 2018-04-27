b50b98c628ff500c89e32ca37074bd1c39eab9d9
Ingrid Laurikainen <ingridlaurikainen@gmail.com>
2018-04-27 09:05:08 +0300

    Lisasin kommentaarid

--- Snake.java
+++ Snake.java
@@ -12,20 +12,18 @@ public class Snake extends Application {
     Group juur = new Group();
     Scene scene = new Scene(juur,600, 600);
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage lava)  {
         Tron game = new Tron();
 
         lava.setTitle( "Tron" );
 
-        //ussi esimeses ruudu tegemine ja selle listi lisamine
+        //Mõlema ussi esimese ruudu tegemine
         liikumine(20,20,570,570);
 
+        //Kontrollimine, kas klaviatuuril on vajutatud kummagi ussi liikumiseks mõeldud nuppu
         scene.setOnKeyPressed(keyEvent -> {
+            //Esimese ussi liikumine
             if (keyEvent.getCode() == KeyCode.UP) {
                 game.yks.setSuund("yles");
             }
@@ -38,7 +36,7 @@ public class Snake extends Application {
             else if (keyEvent.getCode() == KeyCode.RIGHT) {
                 game.yks.setSuund("parem");
             }
-
+            //Teise ussi liikumine
             if (keyEvent.getCode() == KeyCode.W) {
                 game.kaks.setSuund("yles");
             }
@@ -51,10 +49,12 @@ public class Snake extends Application {
             else if (keyEvent.getCode() == KeyCode.D) {
                 game.kaks.setSuund("parem");
             }
+            //Kontrollimine, et kas on võimalik ussil edasi liikuda ja kui on siis tema liigutamine
             game.oota();
             liikumine(game.getX1(), game.getY1(), game.getX2(), game.getY2());
         });
 
+        //Lava suuruse muutmise võimatuks tegemine ja lava kuvamine
         lava.setResizable(false);
         lava.setScene(scene);
         lava.show();
@@ -63,14 +63,17 @@ public class Snake extends Application {
     public void liikumine(int x1, int y1, int x2, int y2) {
         Tron mäng = new Tron();
 
+        //Esimese ussi keha joonistamine
         Rectangle rk1 = new Rectangle(x1,y1,10,10);
         rk1.setFill(Color.GREEN);
-
+        //Teise ussi keha joonistamine
         Rectangle rk2 = new Rectangle(x2,y2,10,10);
         rk2.setFill(Color.RED);
 
+        //Usside kordinaatide lisamine massiivi
         mäng.lisamine(x1, y1, x2, y2);
 
+        //Juurele ristkülikute lisamine, et need oleks laval nähtavad
         juur.getChildren().addAll(rk1, rk2);
     }
 }
--- Tron.java
+++ Tron.java
@@ -14,21 +14,11 @@ public class Tron {
     private int x2 = 570;
     private int y2 = 570;
 
-    public int getX1() {
-        return x1;
-    }
-
-    public int getY1() {
-        return y1;
-    }
-
-    public int getX2() {
-        return x2;
-    }
-
-    public int getY2() {
-        return y2;
-    }
+    //get meetodid, et klassis snake saaks koordinaatide väärtuseid
+    public int getX1() { return x1; }
+    public int getY1() { return y1; }
+    public int getX2() { return x2; }
+    public int getY2() { return y2; }
 
     //Usside koordinaatide massiivi lisamine
     public void lisamine(int x1, int y1, int x2, int y2) {
@@ -36,8 +26,10 @@ public class Tron {
         ussid[x2/10][y2/10]=2;
     }
 
+    //Ussidele algussuuna andmine
+    //Hetkel pole kuskil kasutuses
     private void setSuund() {
-        //Ussidele algussuuna andmine
+
         float rand = (float) Math.random();
         if (rand > 0.5) {
             yks.setSuund("alla");
@@ -48,20 +40,13 @@ public class Tron {
         }
     }
 
-    public String oota() { //Liigutab usse edasi suunas, kuhu nad vaatavad
+    //Annab ussidele uued koordinaadid ja kontrollib, kas ussil on võimalik edasi liikuda
+    public String oota() {
         x1 += yks.getSpeedX();
         y1 += yks.getSpeedY();
         x2 += kaks.getSpeedX();
         y2 += kaks.getSpeedY();
 
-        String seis = stop(x1, y1, x2, y2);
-        if (!seis.equals("")){ //Kontrollime, kas uss tohib edasi liikuda
-            return seis;
-        }
-        return "";
-    }
-
-    private String stop(int x1, int y1, int x2, int y2){ //Usside kokkupõrkamise ja mängualast väljumise kontrollimine
         //Mängualas olemise kontroll
         if ((x1<0 || x1>590 || y1<0 || y1>590)&&(x2<0 || x2>590 || y2<0 || y2>590)) return "Jäite viiki";
         else if (x1<0 || x1>590 || y1<0 || y1>590) return "x1";
