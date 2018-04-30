346737eb647624ebb8be246ce197d6854f657283
KaarelRyysak <kaarelgames@ryysak.com>
2018-04-30 18:57:30 +0300

    Mängu kiiremaks tegemine - debugimisest jäi vana kiirus alles

--- Snake.java
+++ Snake.java
@@ -119,7 +119,7 @@ public class Snake extends Application {
             liikumine(game.getX1(), game.getY1(), game.getX2(), game.getY2());
             Thread thread = new Thread(() -> {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(100);
                 } catch (InterruptedException exc) {
                     throw new Error("Unexpected interruption", exc);
                 }
